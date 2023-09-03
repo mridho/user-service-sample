982c4922c929bcc937022709996d36542c5c096e
Muhammad Ridha <ridho2210@gmail.com>
2023-09-04 00:00:25 +0700

    refactor: tidy up database.sql and gitignore

--- database.sql
+++ database.sql
@@ -1,10 +1,6 @@
 /**
   This is the SQL script that will be used to initialize the database schema.
-  We will evaluate you based on how well you design your database.
-  1. How you design the tables.
-  2. How you choose the data types and keys.
-  3. How you name the fields.
-  In this assignment we will use PostgreSQL as the database.
+  This project use PostgreSQL as the database.
   */
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
